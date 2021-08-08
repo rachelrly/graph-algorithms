a2c469d1c369ff09bca8c9c51e5eb57e207e29df
Rachel Reilly <R.a.reill18@gmail.com>
2021-08-08 17:20:32 -0500

    adds visited value to Node class and method to visit node

--- node.py
+++ node.py
@@ -1,4 +1,8 @@
-class _Node:
+class Node:
     def __init__(self, value, next=None):
         self.value = value
         self.next = next
+        self.visited = False
+
+    def set_node_as_visited(self):
+        self.visited = True
